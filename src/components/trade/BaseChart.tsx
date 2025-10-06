9935026ae6528b7c2314355dc8b8705ada7bf3cc
uros171513 <urosjovanovic171513@gmail.com>
2025-10-06 12:02:59 +0200

    format number values in charts, and y axis labels added

--- src/components/trade/BaseChart.tsx
+++ src/components/trade/BaseChart.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Papa from "papaparse";
 import {
-    BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, LabelList
+    BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, LabelList, Label
 } from "recharts";
 import pluralize from "pluralize";
+import millify from "millify";
 import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
 import { Select, SelectTrigger, SelectContent, SelectItem, SelectValue } from "@/components/ui/select";
 
@@ -19,6 +20,28 @@ type BaseChartProps = {
     csvFile?: string;
     mode?: string;
 };
+
+// Helper functions
+const formatNumber = (value: number) => {
+    const billion = 1_000_000_000;
+    const million = 1_000_000;
+    const thousand = 1_000;
+
+    if (value >= billion) {
+        return (value / billion).toFixed(1) + "B";
+    } else if (value >= million) {
+        return (value / million).toFixed(1) + "M";
+    } else if (value >= thousand) {
+        return (value / thousand).toFixed(1) + "K";
+    } else {
+        return value.toString();
+    }
+}
+
+const capitalizeFirstLetter = (mode) => {
+    const formatted = mode.charAt(0).toUpperCase() + mode.slice(1);
+    return formatted;
+}
 
 export default function BaseChart({ csvFile, mode }: BaseChartProps) {
     const [data, setData] = useState<TradeRecord[]>([]);
@@ -88,12 +111,6 @@ export default function BaseChart({ csvFile, mode }: BaseChartProps) {
 
     }, [data, filteredCountry]);
 
-    // Helper methods
-    const capitalizeFirstLetter = (mode) => {
-        const formatted = mode.charAt(0).toUpperCase() + mode.slice(1);
-        return formatted;
-    }
-
     return (
         <Card className="p-4 space-y-6">
             <CardHeader>
@@ -114,15 +131,26 @@ export default function BaseChart({ csvFile, mode }: BaseChartProps) {
                 </Select>
 
                 {/* Vertical chart */}
-                <BarChart width={800} height={300} data={aggregated} margin={{ top: 20, right: 80, left: 80, bottom: 20 }}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="year" />
-                    <YAxis />
-                    <Tooltip />
-                    <Bar dataKey="total" fill="#3b82f6">
-                        <LabelList dataKey="change" position="top" formatter={(v: any) => `${v}%`} />
-                    </Bar>
-                </BarChart>
+                <div className="mt-10">
+                    <CardTitle>{filteredCountry === "All" ? "Aggregate Import Value per Year (in AED)" : "Aggregate Import Value per Year (in AED) in " + filteredCountry}</CardTitle>
+                    <BarChart width={800} height={300} data={aggregated} margin={{ top: 20, right: 80, left: 80, bottom: 20 }}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="year" />
+                        <YAxis tickFormatter={(value) => millify(value, { precision: 2 })}>
+                            <Label
+                                value="Import Value"
+                                angle={-90}
+                                position="insideLeft"
+                                style={{ textAnchor: "middle", fontSize: 18, fontWeight: 600, fill: "#444" }}
+                                offset={-30}
+                            />
+                        </YAxis>
+                        <Tooltip formatter={(value) => millify(Number(value), { precision: 2 })} />
+                        <Bar dataKey="total" fill="#3b82f6">
+                            <LabelList dataKey="change" position="top" formatter={(v: any) => `${v}%`} />
+                        </Bar>
+                    </BarChart>
+                </div>
 
                 {/* Horizontal chart */}
                 <div className="mt-10">
@@ -135,11 +163,19 @@ export default function BaseChart({ csvFile, mode }: BaseChartProps) {
                         margin={{ top: 20, right: 30, left: 80, bottom: 20 }}
                     >
                         <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis type="number" />
-                        <YAxis dataKey="section" type="category" width={80} />
+                        <XAxis type="number" tickFormatter={(value) => millify(value, { precision: 2 })}/>
+                        <YAxis dataKey="section" type="category" width={80} >
+                              <Label
+                                value="HS Section"
+                                angle={-90}
+                                position="insideLeft"
+                                style={{ textAnchor: "middle", fontSize: 18, fontWeight: 600, fill: "#444" }}
+                                offset={-10}
+                            />
+                        </YAxis>
                         <Tooltip
-                            formatter={(value: any) => value.toLocaleString()}
-                            labelFormatter={(label, payload) => {
+                            formatter={(value: any) => millify(Number(value), { precision: 2 })}
+                            labelFormatter={(label) => {
                                 const found = topSections.find(p => p.section === label);
                                 return found ? `${found.section}: ${found.description}` : label;
                             }}
